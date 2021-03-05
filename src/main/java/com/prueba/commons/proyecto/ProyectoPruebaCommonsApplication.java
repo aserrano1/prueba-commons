4445ea4dd803ca7a06c7329532db3465f71011cf
unknown <jmejia@dcatech.com>
2021-03-05 15:02:49 -0500

    Se realiza la creacion del proyecto commons

---
+++ src/main/java/com/prueba/commons/proyecto/ProyectoPruebaCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.commons.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPruebaCommonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoPruebaCommonsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prueba/commons/proyecto/ProyectoPruebaCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.commons.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPruebaCommonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
